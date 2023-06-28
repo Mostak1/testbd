818577dba6bc461df131ff9d23b194f9e4826f14
Mostak1 <114609802+Mostak1@users.noreply.github.com>
2023-06-28 08:57:20 +0600

    'h'

--- app/Database/Migrations/2023-06-17-111018_ExamsTable.php
+++ app/Database/Migrations/2023-06-17-111018_ExamsTable.php
@@ -17,7 +17,52 @@ class ExamsTable extends Migration
                 'auto_increment' => true,
             ],
 
-            'exam_name' => [
+            'customer_id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+            ],
+            'product_id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+            ],
+            'total' => [
+                'type' => 'FLOAT',
+                'constraint' => '10,2',
+                'null' => true,
+            ],
+            'discount' => [
+                'type' => 'FLOAT',
+                'constraint' => '6,2',
+                'null' => true,
+            ],
+            'quantity' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+            ],
+            'comment' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'payment' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'tranxid' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'status' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'b_address' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            's_address' => [
                 'type'       => 'VARCHAR',
                 'constraint' => 100,
             ],
---
+++ app/Database/Migrations/2023-06-28-022705_OrdersTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
+
+class OrdersTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'   => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+
+            'exam_name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'created_at' => [
+                'type'    => 'TIMESTAMP',
+                'default' => new RawSql('CURRENT_TIMESTAMP'),
+            ]
+
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('Orders');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('orders');
+    }
+}
